74e968654c7f4f468de853c0c957180b3716142b
kimjiae970 <109570479+kimjiae970@users.noreply.github.com>
2023-01-17 17:14:51 +0900

    CI Directory

--- src/main/java/com/example/artizen/controller/MemberController.java
+++ src/main/java/com/example/artizen/controller/MemberController.java
@@ -1,4 +1,13 @@
 package com.example.artizen.controller;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Service
 public class MemberController {
+
+    @GetMapping("/")
+    public String sample() {
+        return "hello";
+    }
 }
