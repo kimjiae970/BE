1291434175ae8cda4f4a931b09f4f6bd2d304af1
kimjiae970 <109570479+kimjiae970@users.noreply.github.com>
2023-01-21 23:30:23 +0900

    CI_check

--- src/main/java/com/example/artizen/controller/MemberController.java
+++ src/main/java/com/example/artizen/controller/MemberController.java
@@ -1,11 +1,13 @@
 package com.example.artizen.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MemberController {
 
-    @GetMapping("/")
-    public String hello() {
-        return "hello";
-    }
+//    @GetMapping("/")
+//    public String hello() {
+//        return "hello";
+//    }
 }
