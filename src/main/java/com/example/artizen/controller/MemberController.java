ffae25f0248b160a3544005fb7facfef74bb35f6
kimjiae970 <109570479+kimjiae970@users.noreply.github.com>
2023-01-25 23:32:46 +0900

    controller

--- src/main/java/com/example/artizen/controller/MemberController.java
+++ src/main/java/com/example/artizen/controller/MemberController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MemberController {
 
-//    @GetMapping("/")
-//    public String hello() {
-//        return "hello";
-//    }
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
 }
